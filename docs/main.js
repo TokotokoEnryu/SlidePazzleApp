8bca35cf70379d327d1e2dcd6f42e8779e9d6a09
TokotokoEnryu <119743186+TokotokoEnryu@users.noreply.github.com>
2022-12-04 02:26:05 +0900

    update main.js
    first commit.

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,17 @@
 // Wrtie your code here.
+var btnStart = document.getElementsByName("btn-start")[0];
+var btnMakeQuiz = document.getElementsByName("btn-makequiz")[0];
+var btnClear = document.getElementsByName("btn-clear")[0];
+
+function main() {
+    btnStart.onclick((e) => {
+        console.log("Hello, start calculation.");
+    });
+    btnMakeQuiz.onclick((e) => {
+        console.log("Hello, start making quiz.");
+    });
+    btnClear.onclick((e) => {
+        console.log("Hello, clear input panel.");
+    })
+}
+main();
