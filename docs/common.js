d2fc1ff5c3ea2c4f020f1e8067b0b7cf1853d0c3
TokotokoEnryu <119743186+TokotokoEnryu@users.noreply.github.com>
2022-12-05 20:56:25 +0900

    add common.js

---
+++ docs/common.js
@@ -0,0 +1,22 @@
+const colorText = {
+    'blank': '空白',
+    'red': '赤',
+    'blue': '青',
+    'green': '緑',
+    // 'yellow': '黄色',
+    'lightgray': '白',
+    // 'black': '黒'
+};
+const colorList = {
+    'red': '#ff0000',
+    'blue': '#0000ff',
+    'green': '#00ff00',
+    // 'yellow': '#ffff00',
+    // 'black': '#000000',
+    'lightgray': '#cccccc'
+};
+const blankColor = '#ffffff';
+
+function noscroll(e) {
+    e.preventDefault();
+};
